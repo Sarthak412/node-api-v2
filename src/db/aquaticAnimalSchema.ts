9a3b99ddb734b0fd523c5ae6e019cbc914cd4000
Sarthak412 <sarthakkamble412@gmail.com>
2023-08-17 15:39:16 +0530

    Initial Commits

---
+++ src/db/aquaticAnimalSchema.ts
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+
+const AquaticAnimalSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please enter the aquatic animal name."]
+        },
+        scientific_name: {
+            type: String,
+            required: [true, "Please enter the scientific name."]
+        },
+        size: {
+            length: {
+                type: String,
+                required: true
+            },
+            weight:{
+                type: String,
+                required: true
+            }
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        interesting_facts: {
+            type: String,
+            required: false
+        },
+        image: {
+            type: String,
+            required: false
+        }
+
+    }, 
+    
+    {
+        timestamps: true
+    }
+)
+
+export const AquaticModel = mongoose.model('AquaAnimal', AquaticAnimalSchema);
+
+
+// * Methods Implementation for AquaticModel
+
+// Fetching all the aquatic animals
+export const getAllAnimals = () => AquaticModel.find();
+
+// Fetching Aquatic Animal using name
+export const getAnimalsByName = (name: string) => AquaticModel.findOne({ name }); 
+
+// Fetching Aquatic animal by id
+export const getAnimalById = (id: string) => AquaticModel.findById(id);
+
+// Method for creating/saving an aquatic animal
+export const createAquaticAnimal = (values: Record <string, any>) => new AquaticModel(values).save().then((animal) => animal.toObject());
+
+// Method for deleting an animal
+const deleteAquaticAnimalById = (id: string) => AquaticModel.findOneAndDelete({_id: id});
+
+// Method for updating an animal
+const updateAquaticAnimal = (id: string, values: Record<string, any>) => AquaticModel.findByIdAndUpdate(id, values);
---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import express from 'express';
+
+import http from 'http';
+
+import bodyParser from 'body-parser';
+
+import cookieParser from 'cookie-parser';
+
+import compression from 'compression';
+
+import dotenv from 'dotenv';
+
+import mongoose from 'mongoose';
+
+import cors from 'cors';
+
+// Environment variables Config
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// MongoDB URL
+const MONGO_URL = process.env.MONGO_URL;
+
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(compression());
+
+app.use(cookieParser());
+
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port http://localhost:${PORT}/`);
+});
+
+mongoose.Promise = Promise;
+
+mongoose.connect(MONGO_URL);
+
+mongoose.connection.on('error', (error: Error) => console.log(error));
