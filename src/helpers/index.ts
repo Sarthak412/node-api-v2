088c6ea8283877e2cc2b5a613fd9e391d24a6be1
Sarthak Kamble <sarthakkamble412@gmail.com>
2023-08-20 18:11:19 +0530

    Initial Commits

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -6,4 +6,4 @@ export const random = () => crypto.randomBytes(128).toString('base64');
 
 export const authentication = (salt: string, password: string) => {
     return crypto.createHmac('sha256', [salt, password].join('/')).update(secret).digest('hex');
-}
+}   
